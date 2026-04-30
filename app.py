393a30db6d9c9b33e6b8a387811bd1096e23fffa
Lucas Ferreira <lucas.fertony@gmail.com>
2026-04-30 17:16:02 -0300

    desafio 4

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write(b'hello world')
+
+server = HTTPServer(('0.0.0.0', 9999), Handler)
+print("Server running on port 9999")
+server.serve_forever()
