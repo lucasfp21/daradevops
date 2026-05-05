ae922262d11835cba5cdea920e31e003d8302b70
Lucas Ferreira <lucas.fertony@gmail.com>
2026-05-05 20:41:09 -0300

    upgrade hello-python to v2.0

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(b'hello world')
+        self.wfile.write(b'hello world v2')
 
 server = HTTPServer(('0.0.0.0', 9999), Handler)
 print("Server running on port 9999")
