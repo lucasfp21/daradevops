d8abc2af7d25c5bff90febcaca976656206211e6
Lucas Ferreira <lucas.fertony@gmail.com>
2026-05-08 16:51:08 -0300

    add kubernetes secret support

--- app/app.py
+++ app/app.py
@@ -1,14 +1,24 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 
-APP_NAME = os.getenv("APP_NAME", "default-app")
-APP_AUTHOR = os.getenv("APP_AUTHOR", "unknown")
-APP_VERSION = os.getenv("APP_VERSION", "0")
+APP_NAME = os.getenv("APP_NAME")
+APP_AUTHOR = os.getenv("APP_AUTHOR")
+APP_VERSION = os.getenv("APP_VERSION")
+APP_TOKEN = os.getenv("APP_TOKEN")
 
-MESSAGE = f"""
+CONFIG_OK = all([APP_NAME, APP_AUTHOR, APP_VERSION])
+
+if CONFIG_OK:
+    MESSAGE = f"""
 App: {APP_NAME}
 Author: {APP_AUTHOR}
 Version: {APP_VERSION}
+
+Secret carregado: {'SIM' if APP_TOKEN else 'NAO'}
+"""
+else:
+    MESSAGE = """
+ERRO: ConfigMap nao carregado corretamente.
 """
 
 class Handler(BaseHTTPRequestHandler):
